6ef7fa1448707a197a6819a6b1af8e80d459d994
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2018-03-03 18:46:51 +0200

    add(app.component): add router links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { DirectoryComponent } from './directory/directory.component'
+import { DefaultComponent } from './default/default.component'
 import { HomeComponent } from './home/home.component'
   
 export const APP_ROUTES: Routes = [
     {path:'directory', component: DirectoryComponent},
+    {path:'default', component: DefaultComponent},
     {path:'', component: HomeComponent}
 ];
